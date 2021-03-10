93bab0fcfbd7aa8c3e3cb2bf305d43cb9e351c68
bahleg <bakhteev@phystech.edu>
2021-03-10 23:25:26 +0300

    Added dataset size parameter

--- code/cifar_dataset.py
+++ code/cifar_dataset.py
@@ -5,7 +5,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
 
-def cifar10_loader(data_path='../data', batch_size=128):
+def cifar10_loader(data_path='../data', batch_size=128, maxsize=-1):
     """
     Loads the cifar10 dataset in torch-ready format
     :param data_path:
@@ -15,24 +15,28 @@ def cifar10_loader(data_path='../data', batch_size=128):
 
     mean = [x / 255 for x in [125.3, 123.0, 113.9]]
     std = [x / 255 for x in [63.0, 62.1, 66.7]]
-
+    
     train_transform = transforms.Compose(
         [transforms.RandomHorizontalFlip(),
          transforms.RandomCrop(32, padding=4),
          transforms.ToTensor(),
          transforms.Normalize(mean, std)])
     test_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)])
-
+    
     train_data = dset.CIFAR10(data_path, train=True, transform=train_transform, download=True)
     train_data_original = dset.CIFAR10(data_path, train=True, transform=test_transform, download=True)
+    if maxsize > 0:
+        train_data = torch.utils.data.Subset(train_data, list(range(maxsize)))
+        train_data_original = torch.utils.data.Subset(train_data_original, list(range(maxsize)))  
+        
     test_data = dset.CIFAR10(data_path, train=False, transform=test_transform, download=True)
 
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=False, num_workers=2,
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=False, num_workers=0,
                                                pin_memory=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=2,
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=0,
                                               pin_memory=True)
     train_loader_original = torch.utils.data.DataLoader(train_data_original, batch_size=batch_size,
-                                                        shuffle=False, num_workers=2, pin_memory=True)
+                                                        shuffle=False, num_workers=0, pin_memory=True)
 
     return train_loader, test_loader, train_loader_original
 
